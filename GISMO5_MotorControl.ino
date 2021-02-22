e5c497814566a53efebc74deb07104b33719c147
GRIETIOTLAB <77530550+GRIETIOTLAB@users.noreply.github.com>
2021-02-22 14:33:27 +0530

    Add files via upload

---
+++ GISMO5_MotorControl.ino
@@ -0,0 +1,14 @@
+#define motorPin 13
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(motorPin,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(motorPin,HIGH);
+delay(2000);
+digitalWrite(motorPin,LOW);
+delay(2000);
+}
