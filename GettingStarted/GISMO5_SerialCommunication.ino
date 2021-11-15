fa333a0a2e4c5c0ddc4ade3b6751119473cbeba3
GRIETIOTLAB <77530550+GRIETIOTLAB@users.noreply.github.com>
2021-11-15 15:45:15 +0530

    Add files via upload

---
+++ GettingStarted/GISMO5_SerialCommunication.ino
@@ -0,0 +1,14 @@
+int count;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.println(count);
+count++;
+if(count == 10) count = 0;
+delay(1000);
+}
